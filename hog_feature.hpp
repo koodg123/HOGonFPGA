a2dff7fef907138988786abd83512625b82e7466
Khushal Sethi <iaskhushal@gmail.com>
2018-06-29 00:11:11 +0530

    changes to modify and make more interactive

--- hog_feature.hpp
+++ hog_feature.hpp
@@ -1,7 +1,7 @@
 
 
-#ifndef _FPGA_TOP_H_
-#define _FPGA_TOP_H_
+#ifndef _HOG_FEATURE_H_
+#define _HOG_FEATURE_H_
 
 #include <cassert>
 #include <cmath>
@@ -18,17 +18,10 @@
 const int NUM_IMG_CACHE_LINES = 4;
 // Number of Processing Elements
 const int N_PE = 8;
-size_t blocksize;
-size_t cellsize;
-size_t stride;
-size_t grad_type; ///< "signed" (0..360) or "unsigned" (0..180) gradient
-size_t binning; ///< the number of bins for each cell-histogram
-size_t bin_width; ///< size of one bin in degree
-size_t n_cells_per_block_y = blocksize/_cellsize;
-size_t n_cells_per_block_x = n_cells_per_block_y;
-size_t n_cells_per_block = n_cells_per_block_y*_n_cells_per_block_x;
-size_t block_hist_size = binning*_n_cells_per_block;
-size_t stride_unit = stride/cellsize;
+size_t blocksize; size_t cellsize; size_t stride; size_t grad_type; size_t binning;
+size_t bin_width; size_t n_cells_per_block_y = blocksize/_cellsize;
+size_t n_cells_per_block_x = n_cells_per_block_y; size_t n_cells_per_block = n_cells_per_block_y*_n_cells_per_block_x;
+size_t block_hist_size = binning*_n_cells_per_block; size_t stride_unit = stride/cellsize;
 // ====================
 // = Type Definitions =
 // ====================
@@ -36,6 +29,7 @@ typedef ap_uint<2> cacheline_t;  // cache height = 3 or 4 lines
 typedef ap_uint<NBITS(MAX_INPUT_PER_LAYER)> imgdramoffset_t;
 typedef ap_uint<NBITS(MAX_IMAGE_CACHE_SIZE)> imgcacheaddr_t;
 typedef ap_uint<NBITS(MAX_IMAGE_CACHE_SIZE / 4)> pixelperrow_t;
-
 typedef ap_int<NBITS(MAX_DIMENSION) + 2> coordinate_t;
+
+
 
